bff39453fbbaf40a72fb533861cb29cc31128c09
Kanako Kobayashi <kanako.kobayashi@lifeistech.co.jp>
2015-08-08 07:12:10 +0900

    サーバのデータを取得、更新処理を追加

--- tickleApp/ViewController.swift
+++ tickleApp/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    let requestUrl = "http://localhost:4567/"
+    var titleArray = NSArray()
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var inputText: UITextField!
+    
+    /*
+    * メッセージを表示する処理
+    * サーバから、メッセージを取得します。
+    */
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 通常のGetリクエスト
+        //Alamofire.request(.GET, requestUrl)
+        // 非同期のGetリクエスト
+        Alamofire.request(.GET, requestUrl).responseJSON
+            {(request, response, json, error) in
+            let jsonDic = json as! NSDictionary
+            let responseData = jsonDic["responseData"] as! NSDictionary
+            self.titleArray = responseData["results"] as! NSArray
+            self.titleLabel.text = self.titleArray[0]["title"] as? String
+            self.messageLabel.text = self.titleArray[0]["message"] as? String
+        }
     }
-
+    
+    /*
+    * メッセージを保存する処理
+    * サーバに、メッセージを送信します。
+    */
+    @IBAction func sendMessage(sender: AnyObject) {
+        println(self.inputText.text)
+        let parameters = ["comment": self.inputText.text]
+        Alamofire.request(.POST, requestUrl + "/new", parameters: parameters)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
 
